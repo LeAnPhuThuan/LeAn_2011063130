7ba25bc25f1b3cf41e1b4545ac80a6d85e470fc5
LeAn <antrumso2002@gmail.com>
2023-03-27 09:22:46 +0700

    tuan so 3

---
+++ LeAn_2011063130/Migrations/202303251654376_Khoi Tao Bang.cs
@@ -0,0 +1,16 @@
+﻿namespace LeAn_2011063130.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class KhoiTaoBang : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
